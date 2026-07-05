ad0ab8c772cd4c3df204966684e40d2e52ef4a60
vaishu0077 <vaishnavikumaresn@gmail.com>
2026-07-05 18:26:04 +0530

    Update and rename Create Country Table to Create Country Table.sql

---
+++ Week 2/Spring Data JPA with Spring Boot, Hibernate/Spring Data JPA - Quick Example/Create Country Table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE country(
+    code VARCHAR(2) PRIMARY KEY,
+    name VARCHAR(50)
+);
+INSERT INTO country VALUES
+('IN','India');
+
+INSERT INTO country VALUES
+('US','United States of America');
