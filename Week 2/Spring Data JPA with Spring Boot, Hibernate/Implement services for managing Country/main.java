4a46a4b816062d600b73f835d21f7770c1378744
vaishu0077 <vaishnavikumaresn@gmail.com>
2026-07-06 10:33:40 +0530

    Create main.java

---
+++ Week 2/Spring Data JPA with Spring Boot, Hibernate/Implement services for managing Country/main.java
@@ -0,0 +1,8 @@
+@SpringBootApplication
+public class CountryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CountryServiceApplication.class, args);
+    }
+
+}
