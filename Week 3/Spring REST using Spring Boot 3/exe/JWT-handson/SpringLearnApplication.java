d5d7639fd7bd784ab0eae395ec96f8e9e815d391
vaishu0077 <vaishnavikumaresn@gmail.com>
2026-07-06 15:53:09 +0530

    Create SpringLearnApplication.java

---
+++ Week 3/Spring REST using Spring Boot 3/exe/JWT-handson/SpringLearnApplication.java
@@ -0,0 +1,29 @@
+package com.cognizant.springlearn;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SpringLearnApplication {
+    public static void displayDate() throws Exception {
+
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("date-format.xml");
+
+        SimpleDateFormat format =
+                context.getBean("dateFormat", SimpleDateFormat.class);
+
+        Date date = format.parse("31/12/2018");
+
+        System.out.println(date);
+
+    }
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SpringLearnApplication.class, args);
+        displayDate();
+    }
+}
